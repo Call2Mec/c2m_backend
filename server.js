a92a2dbd801a75fd6c7c6a6be33dfad6ddc8431f
Call2mec <call2mec@gmail.com>
2022-07-06 01:14:44 +0530

    port change

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ dotenv.config({ path: "./config.env" });
 import app from "./app.js";
 import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 3018;
+const PORT = process.env.PORT || 3018
 // const DB_URL = process.env.DB_URL + process.env.DB_COLLECTION;
 const DB_URI = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.depssen.mongodb.net/${process.env.DB_COLLECTION}?retryWrites=true&w=majority`;
 mongoose
   .connect(DB_URI)
   .then(() => {
     console.log("database connected");
-    app.listen(process.env.PORT || 8080 , () => {
+    app.listen(PORT, () => {
       console.log(`Server is up and running on Port : ${PORT}`);
     });
   })
